edc9e2f065f91cbd4e5e38270a68d58ad9f78998
ardumez <ardumez@outlook.fr>
2016-02-27 22:11:35 +0100

    InsertionSort & QuickSort

---
+++ Sorting/Insertion Sort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] values = new int[]{8, 10, 1, 5, 7};
+
+            for (int increment = 0; increment < values.Length -1; increment++)
+            {
+                // Move on left while current is min of left values
+                for (int decrement = increment; decrement >= 0; decrement--)
+                {
+                    int rightValue = values[decrement + 1];
+                    int currentValue = values[decrement];
+                    
+                    if (currentValue > rightValue)
+                    {
+                        values[decrement] = rightValue;
+                        values[decrement + 1] = currentValue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+                Debug(values);
+            }
+        }
+        
+        public static void Debug(int[] values)
+        {
+            Console.WriteLine("-------------------");
+            for (int i = 0; i < values.Length; i ++)
+            {
+                Console.Write(values[i].ToString() + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
